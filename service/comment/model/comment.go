0ea9ed34a9be41060c0b6e4f44bff6dd68f4f0ac
chaoranz758 <2715584135@qq.com>
2023-02-28 20:38:23 +0800

    update readme

--- service/comment/model/comment.go
+++ service/comment/model/comment.go
@@ -6,7 +6,7 @@ import (
 )
 
 type Commit struct {
-	CommitID  int64        `gorm:"type:bigint(20);not null;uniqueIndex:commit;comment:'评论ID'" json:"commitID"`
+	CommitID  int64        `gorm:"primarykey;type:bigint(20);not null;uniqueIndex:commit;comment:'评论ID'" json:"commitID"`
 	VideoID   int64        `gorm:"type:bigint(20);not null;index:video;comment:'该评论所属的视频ID'" json:"videoID"`
 	UserID    int64        `gorm:"type:bigint(20);not null;index:user;comment:'评论创建用户ID'" json:"userID"`
 	Message   string       `gorm:"type:varchar(8192);not null;comment:'评论内容'" json:"message"`
